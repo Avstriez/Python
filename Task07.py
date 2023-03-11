35f78565c8cbc8b9d146cd94bd9d387030009f08
Avstriez <alexej.terehin@mail.ru>
2023-03-11 18:56:46 +0300

    finished2 task07

--- Task07.py
+++ Task07.py
@@ -7,7 +7,7 @@
 # Output: YES
 
 n = int(input())
-if ((n % 4 == 0 and n % 100 != 0) or (n % 400 == 0)):
+if ((n % 4 == 0) and (n % 100 != 0)) or (n % 400 == 0):
     print('Високосный')
 else:
     print('Обычный')
