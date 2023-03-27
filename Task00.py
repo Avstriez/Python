c53f42db679210263202421120f221efe07701c1
Avstriez <alexej.terehin@mail.ru>
2023-03-27 16:00:56 +0300

    finished task31

--- Task00.py
+++ Task00.py
@@ -1,18 +1,50 @@
-n = int(input('Enter N: '))
-m = int(input('Enter M: '))
+# Фибоначчи:
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+list_1 = []
+for i in range(1, 10):
+    list_1.append(fib(i - 2))
+print(list_1) # [1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-array_1 = []
-print('Enter N-numbers: ')
-for i in range(n):
-    array_1.append(int(input()))
+# Быстрая сортировка:
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot    = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+print(quicksort([10, 5, 2, 3]))
 
-array_2 = []
-print('Enter M-numbers: ')
-for i in range(m):
-    array_2.append(int(input()))
+# Сортировка слиянием:
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
 
-set_1 = set(array_1)
-set_2 = set(array_2)
-set_result = set_1.intersection(set_2)
-print('Во введенных наборах встречаются числа: ', end='')
-print(*sorted(set_result))
+nums = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(nums)
+print(nums)
